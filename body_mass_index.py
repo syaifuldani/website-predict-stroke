2580b0f9831af5cc5515b5622d7b2346ca8ce7db
Syaiful Rochmandani <syaifulrochmandani737@gmail.com>
2024-11-16 15:00:03 +0700

    add .Gitignore file

--- body_mass_index.py
+++
@@ -1,5 +0,0 @@
-beratbadan = float(input('Inputkan berat badan anda :'))
-tinggibadan = float(input('Inputkan tinggibadan anda :'))
-
-bmi = beratbadan / (tinggibadan * tinggibadan)
-print("ini merupakan BMI anda", bmi)
